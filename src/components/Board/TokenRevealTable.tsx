619a8d435430644706edba4f42dc4be30da650ac
Kushal Nandwani <kushalnandwani98@gmail.com>
2024-06-13 23:04:13 +0530

    fix ts

--- src/components/Board/TokenRevealTable.tsx
+++ src/components/Board/TokenRevealTable.tsx
@@ -18,7 +18,7 @@ export const TokenRevealTable = (props: TableProps) => {
       )}
     >
       <ul className="grid sm:grid-cols-9 grid-cols-6">
-        {getRevealedTokenGrid.map((token, i) => {
+        {getRevealedTokenGrid.map((token: any, i: number) => {
           return (
             <li
               className="outline-double outline-primary outline-offset-2 text-secondary flex items-center justify-center font-bold text-2xl h-16 w-16"
